c3e808d487c2a512bbbbd22c3d8326c3590329de
unknown <jueeli97@gmail.com>
2020-07-30 23:04:56 +0530

    Type Conversion
    Signed-off-by: unknown <jueeli97@gmail.com>

---
+++ main.js
@@ -0,0 +1,68 @@
+
+
+// Number DataType
+let numValue1 = 10;
+
+
+// Number to String Conversion
+let strValue1 = String(numValue1);
+console.log(strValue1); // 10
+console.log(typeof(strValue1)); // string
+
+// Number to Boolean Conversion
+let boolValue1 = Boolean(numValue1);
+console.log(boolValue1);
+console.log(typeof(boolValue1));
+
+
+
+
+// String DataType
+
+let strValue2 = undefined;
+
+
+// String to Number Conversion
+ let numValue2 = Number(strValue2);
+ console.log(numValue2); // NaN
+ console.log(typeof(numValue2)); // Number
+
+ //String to Boolean Conversion
+ let boolValue2 = Boolean(strValue2);
+ console.log(boolValue2); 
+ console.log(typeof(boolValue2)); // boolean
+
+
+ // Boolean datatype
+  let boolValue3= true;
+
+
+  // Boolean to Number conversion
+
+ let numValue3 = Number(boolValue3);
+console.log(numValue3);// 1
+ console.log(typeof(numValue3)); // number
+
+ // Boolean to String Conversion
+
+  let strValue3 = String(boolValue3);
+  console.log(strValue3); // true
+ console.log(typeof(strValue3)); // string
+
+
+
+
+let strValue4 = "10";
+console.log(Number(strValue3));// NaN
+console.log(Boolean(Number(strValue3)));// false
+console.log(typeof(Boolean(Number(strValue3)))); // Boolean
+
+
+
+
+
+
+
+
+
+
